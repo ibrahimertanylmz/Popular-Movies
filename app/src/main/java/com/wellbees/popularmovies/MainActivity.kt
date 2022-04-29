df5a0c6ab37ee7ab65db1236c7b77e15ce3f1c4c
ibrahimertanylmz <ibrahimertanylmz@gmail.com>
2022-04-29 18:19:00 +0300

    basic app configurations

--- app/src/main/java/com/wellbees/popularmovies/MainActivity.kt
+++ app/src/main/java/com/wellbees/popularmovies/MainActivity.kt
@@ -2,10 +2,24 @@ package com.wellbees.popularmovies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.wellbees.popularmovies.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+
+
+
+
+
+
+
     }
 }

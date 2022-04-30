f1d5e26c95cf71700f846a5c9c363bee0c5ef94a
ibrahimertanylmz <ibrahimertanylmz@gmail.com>
2022-04-30 18:04:14 +0300

    adapter and viewholder added for person

---
+++ app/src/main/java/com/wellbees/popularmovies/adapter/PersonAdapter.kt
@@ -0,0 +1,30 @@
+package com.wellbees.popularmovies.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.wellbees.popularmovies.databinding.PersonItemBinding
+import com.wellbees.popularmovies.model.Person
+import com.wellbees.popularmovies.viewholder.MovieViewHolder
+import com.wellbees.popularmovies.viewholder.PersonViewHolder
+
+class PersonAdapter(
+    var context: Context,
+    var liste: ArrayList<Person>,
+    var itemClick: (position: Int) -> Unit
+) : RecyclerView.Adapter<PersonViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PersonViewHolder {
+        val from = LayoutInflater.from(context)
+        val binding = PersonItemBinding.inflate(from, parent, false)
+        return PersonViewHolder(context, binding, itemClick)
+    }
+
+    override fun onBindViewHolder(holder: PersonViewHolder, position: Int) {
+        holder.bindData(liste.get(position))
+    }
+
+    override fun getItemCount(): Int {
+        return liste.size
+    }
+}
---
+++ app/src/main/java/com/wellbees/popularmovies/viewholder/PersonViewHolder.kt
@@ -0,0 +1,32 @@
+package com.wellbees.popularmovies.viewholder
+
+import android.content.Context
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.wellbees.popularmovies.R
+import com.wellbees.popularmovies.databinding.PersonItemBinding
+import com.wellbees.popularmovies.model.Person
+
+class PersonViewHolder(
+    var context: Context,
+    private val binding: PersonItemBinding,
+    var itemClick: (position: Int) -> Unit
+) : RecyclerView.ViewHolder(binding.root) {
+
+    val baseUrl: String = "https://image.tmdb.org/t/p/original/"
+
+    init {
+        itemView.setOnClickListener {
+            itemClick(adapterPosition)
+        }
+    }
+
+    fun bindData(person: Person) {
+        Glide.with(context)
+            .load(baseUrl + person.profilePath)
+            .placeholder(R.drawable.ic_loading)
+            .error(R.drawable.ic_error_loading_image)
+            .into(binding.imagePerson);
+        binding.tvName.text = person.name
+    }
+}
